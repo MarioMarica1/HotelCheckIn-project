a4474a7e49a2127645afd1fe8099f86adb0f7416
Luca <mihetpopaluca@gmail.com>
2026-01-18 16:26:00 +0200

    -Clarificare logica ILogger -Imbunatatirea metodelor Log si IsEnabled, metode necesare prin contract

--- Hosta_Hotel/Logging/JsonFileLogger.cs
+++ Hosta_Hotel/Logging/JsonFileLogger.cs
@@ -6,7 +6,7 @@ namespace Hosta_Hotel.Logging;
 public class JsonFileLogger : ILogger
 {
     private readonly string _filePath;
-    private static readonly object _lock = new object();
+    private static readonly object _fileLock = new object(); // e un token, vezi linia 36
 
     public JsonFileLogger(string path)
     {
@@ -15,23 +15,27 @@ public class JsonFileLogger : ILogger
 
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
 
-    public bool IsEnabled(LogLevel logLevel) => true;
+    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
         if (formatter == null) return;
 
+        // Obiect anonim pentru serializare
         var logEntry = new
         {
             Timestamp = DateTime.Now,
             Level = logLevel.ToString(),
+            EventId = eventId.Id,
             Message = formatter(state, exception),
             Exception = exception?.Message
         };
 
         string jsonLine = JsonSerializer.Serialize(logEntry);
 
-        lock (_lock)
+        // Compilatorul traduce asta ca fiind un "try-finally"
+        // in clasa Monitor (Monitor.Enter/Exit)
+        lock (_fileLock)
         {
             File.AppendAllText(_filePath, jsonLine + Environment.NewLine);
         }
